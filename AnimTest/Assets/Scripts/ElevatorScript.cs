5e2c349c413a1150a23c34915778ef689c159085
Souraphath <w.souraphath@gmail.com>
2015-10-20 23:49:22 -0500

    Added elevator animation

---
+++ AnimTest/Assets/Scripts/ElevatorScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorScript : MonoBehaviour
+{
+	Animator animator;
+	
+	void Awake(){
+		animator = GetComponent<Animator> ();
+	}
+	
+	void OnTriggerEnter(){
+		animator.SetTrigger ("isPlayer");
+	}
+}
