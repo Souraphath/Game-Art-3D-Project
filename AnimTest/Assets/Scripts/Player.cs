3303b33f1da4a554ab4e93ce86015845f9e34148
zerorise <zerorise.patel@gmail.com>
2015-10-29 12:34:57 -0500

    added falling animation to zero, fixed his walking problem

--- AnimTest/Assets/Scripts/Player.cs
+++ AnimTest/Assets/Scripts/Player.cs
@@ -8,13 +8,13 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;
     public GameObject cameraObject;
     CameraScript cameraScript;
-
+	Collision col;
     Animator anim;
 
     public CharacterController cc;
 
     float forwardSpeed;
-    float sideSpeed;
+    float sideSpeed=0f;
     float verticalVelocity;
     float jumpSpeed = 7.9f;
 
@@ -23,15 +23,14 @@ public class Player : MonoBehaviour
         //Cursor.visible = false;
         cc = GetComponent<CharacterController>();
         cameraScript = cameraObject.GetComponent<CameraScript>();
-        verticalVelocity = 0f;
         anim = GetComponent<Animator>();
     }
-    void LateUpdate()
+    void Update()
     {
         transform.rotation = Quaternion.Euler(0, cameraObject.GetComponent<CameraScript>().curYRot, 0);
         bool in2DMode = cameraScript.in2DMode;
         // movement
-        sideSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+
 
         if (in2DMode) {
             forwardSpeed = 0;
@@ -43,10 +42,16 @@ public class Player : MonoBehaviour
             forwardSpeed = Input.GetAxis("Vertical") * moveSpeed;
 
         if(!cc.isGrounded)
-            verticalVelocity += Physics.gravity.y * 1.5f * Time.deltaTime;
+            verticalVelocity += Physics.gravity.y *1.5f* Time.deltaTime;
 
+		if (verticalVelocity<-2)
+			anim.SetBool ("Falling",true);
+
+		print (verticalVelocity);
         if (cc.isGrounded) {
 			anim.SetBool("Jump",false);
+			verticalVelocity=0;
+			anim.SetBool ("Falling",false);
 		}
 
         if (cc.isGrounded && Input.GetButtonDown("Jump"))
@@ -57,18 +62,20 @@ public class Player : MonoBehaviour
 
         Vector3 velocity = new Vector3(sideSpeed, verticalVelocity, forwardSpeed);
         velocity = transform.rotation * velocity;
+		if (Input.GetAxis ("Horizontal") < -0.1f) {
+			sideSpeed = -1f * moveSpeed;
+			transform.localScale = new Vector3 (-3, 3, 3);
+		} if (Input.GetAxis ("Horizontal") > 0.1f) {
+			sideSpeed = moveSpeed;
+			transform.localScale = new Vector3 (3, 3, 3);
+		} if(Input.GetAxis ("Horizontal") ==0f)
+			sideSpeed = 0.01f;
         if(sideSpeed != 0)
             anim.SetFloat("Walk", Mathf.Abs(sideSpeed));
         if(forwardSpeed != 0)
             anim.SetFloat("Walk", Mathf.Abs(forwardSpeed));
 
         cc.Move(velocity * Time.deltaTime);
-		if (Input.GetAxis ("Horizontal") < -0.1f) {
-			transform.localScale=new Vector3(-3,3,3);
-		}
-		if (Input.GetAxis ("Horizontal") > 0.1f) {
-			transform.localScale=new Vector3(3,3,3);
-		}
 		if(Input.GetButtonDown("Fire1"))
 			anim.SetBool("Slash",true);
 		if(Input.GetButtonDown("Fire2"))
