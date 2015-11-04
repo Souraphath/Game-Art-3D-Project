c9575dbe1daf1f2d1e25dce73d82fb94c974874d
Souraphath <w.souraphath@gmail.com>
2015-11-04 02:11:39 -0600

    Added trigger for gun animation; detects player

---
+++ AnimTest/Assets/Scripts/HelicopterScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicopterScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			GameObject gun = transform.Find ("Gun").gameObject;
+			gun.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Player") {
+			GameObject gun = transform.Find ("Gun").gameObject;
+			gun.SetActive (false);
+		}
+	}
+}
