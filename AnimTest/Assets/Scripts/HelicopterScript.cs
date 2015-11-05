6e989efffd831db393b539617bc44aad9cf8f956
Souraphath <w.souraphath@gmail.com>
2015-11-04 23:19:28 -0600

    Added new helicopter animations
    Sprite sheet edited in photoshop

--- AnimTest/Assets/Scripts/HelicopterScript.cs
+++ AnimTest/Assets/Scripts/HelicopterScript.cs
@@ -3,27 +3,27 @@ using System.Collections;
 
 public class HelicopterScript : MonoBehaviour {
 
+	Animator anim;
+
 	// Use this for initialization
 	void Start () {
-
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Player") {
-			GameObject gun = transform.Find ("Gun").gameObject;
-			gun.SetActive (true);
+			anim.SetTrigger ("isPlayer");
 		}
 	}
 
 	void OnTriggerExit(Collider other){
 		if (other.tag == "Player") {
-			GameObject gun = transform.Find ("Gun").gameObject;
-			gun.SetActive (false);
+			anim.SetTrigger ("isPlayer");
 		}
 	}
 }
