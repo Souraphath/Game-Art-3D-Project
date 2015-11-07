ddc4565a2c88311b2541b11451ce38ba1d78e908
Souraphath <w.souraphath@gmail.com>
2015-11-07 01:04:24 -0600

    Fixed helicopter animations and collider
    Collider previously rotated with the helicopter; Collider now stays in
    its position when the camera is rotating

---
+++ AnimTest/Assets/Scripts/ColliderScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderScript : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponentInChildren<Animator> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			anim.SetTrigger ("isPlayer");
+		}
+	}
+	
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Player") {
+			anim.SetTrigger ("isPlayer");
+		}
+	}
+}
--- AnimTest/Assets/Scripts/HelicopterScript.cs
+++ AnimTest/Assets/Scripts/HelicopterScript.cs
@@ -11,21 +11,25 @@ public class HelicopterScript : MonoBehaviour {
 	void Start () {
 		anim = GetComponent<Animator> ();
 		cameraScript = cameraObject.GetComponent<CameraScript>();
+		anim.SetBool ("is2DMode", false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = Quaternion.Euler(0, cameraObject.GetComponent<CameraScript>().curYRot, 0);
+		transform.rotation = Quaternion.Euler (0, cameraObject.GetComponent<CameraScript> ().curYRot, 0);
 		bool in2DMode = cameraScript.in2DMode;
 
 		if (in2DMode) {
 			Vector3 curPos = transform.position;
-			Vector3 newPos = curPos - new Vector3(0, 0, 7 + curPos.z);
+			Vector3 newPos = curPos - new Vector3 (0, 0, 7 + curPos.z);
 			transform.position = newPos;
+			anim.SetBool ("is2DMode", true);
+		} else {
+			anim.SetBool ("is2DMode", false);
 		}
 	}
 
-	void OnTriggerEnter(Collider other){
+	/*void OnTriggerEnter(Collider other){
 		if (other.tag == "Player") {
 			anim.SetTrigger ("isPlayer");
 		}
@@ -35,5 +39,5 @@ public class HelicopterScript : MonoBehaviour {
 		if (other.tag == "Player") {
 			anim.SetTrigger ("isPlayer");
 		}
-	}
+	}*/
 }
