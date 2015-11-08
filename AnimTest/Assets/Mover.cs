4408d421e4a0bf8a15628b9a8052456b6c123220
Souraphath <w.souraphath@gmail.com>
2015-11-07 18:09:10 -0600

    Added heli projectiles; shoots when trigger activates

---
+++ AnimTest/Assets/Mover.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+	Rigidbody rigidbody;
+	public GameObject cameraObject;
+	CameraScript cameraScript;
+	public Transform target;
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		//GameObject go = GameObject.FindGameObjectWithTag ("Player");
+		//target = go.transform;
+		//myTransform.LookAt (target);
+		rigidbody = GetComponent<Rigidbody> ();
+		//cameraScript = cameraObject.GetComponent<CameraScript>();
+		//rigidbody.velocity = transform.forward * speed;
+		rigidbody.velocity = Vector3.left * speed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//float amtToMove = 20 * Time.deltaTime;
+		//myTransform.Translate (Vector3.forward * amtToMove);
+		/*bool in2DMode = cameraScript.in2DMode;
+
+		if (in2DMode) {
+			//rigidbody.velocity = transform.forward * speed;
+		} else {
+			//rigidbody.velocity = transform.forward * speed;
+		}*/
+	}
+}
--- AnimTest/Assets/Scripts/ColliderScript.cs
+++ AnimTest/Assets/Scripts/ColliderScript.cs
@@ -3,19 +3,36 @@ using System.Collections;
 
 public class ColliderScript : MonoBehaviour {
 	Animator anim;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	
+	private float nextFire;
+
+	public bool playerInCollider;
 	// Use this for initialization
 	void Start () {
 		anim = GetComponentInChildren<Animator> ();
+		playerInCollider = false;
+	}
+
+	void Update(){
+		if (playerInCollider && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Player") {
+			playerInCollider = true;
 			anim.SetTrigger ("isPlayer");
 		}
 	}
 	
 	void OnTriggerExit(Collider other){
 		if (other.tag == "Player") {
+			playerInCollider = false;
 			anim.SetTrigger ("isPlayer");
 		}
 	}
